1b6e6005a83ddd0836807d5b4f1627ed8b44447d
ramya <105221239+Ramya1401@users.noreply.github.com>
2024-04-23 22:39:28 +0530

    Update QuizData.js

--- Version 1/src/Data/QuizData.js
+++ Version 1/src/Data/QuizData.js
@@ -1,22 +1,22 @@
 export const QuizData = [
     {
-        question: "Which language runs in a web browser?",
-        options:[ "Java", "C", "Python", "JavaScript"],
+        question: "Applications and services that run on a distributed network using virtualized resources is known as",
+        options:[ "Parallel computing", "Soft computing", "Distributed computing", "Cloud computing"],
         answer: 4
     },
     {
-        question: "What does CSS stand for?",
-        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
-        answer: 2
+        question: "Which of the following acronyms refers to a software distribution model in which a cloud provider manages and hosts an app that uses access via the internet?",
+        options:["IaaS", "PaaS", "SaaS", "None of the above"],
+        answer: 3
     },
     {
-        question: "What does HTML stand for?",
-        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        question: "What is the primary purpose of a hypervisor in virtualization?",
+        options: ["To allocate hardware resources and manage VMs", "To create virtual hard disks", "To manage network connections", "To manage the guest OS"],
         answer: 1
     },
     {
-        question: "What year was JavaScript launched?",
-        options: ["1996", "1995", "1994", "none of the above"],
-        answer:2
+        question: "Information on how to build a docker image is written in a file named:",
+        options: ["Docker file", "Jenkins file", "Docker compose", "Docker image"],
+        answer:1
     },
 ];
